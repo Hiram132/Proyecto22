295b7b8851a348d89dec4ae2abf8d247ef1aba2a
Hiram132 <112664762+Hiram132@users.noreply.github.com>
2022-10-30 13:30:02 -0600

    Add files via upload

---
+++ Plantilla-del-proyecto-C22-main/sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var player, playerBase, playerArcher;
+var baseimage;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+
+  //crear el cuerpo base del jugador
+  //crear el cuerpo del jugador
+  playerBase = new PlayerBase(300, 500, 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(340,playerBase.body.position.y - 180,120,120);
+
+
+}
+
+function draw() {
+  background(backgroundImg);
+
+  //mostrar la imagen del jugador utilizando la función image()
+  //mostrar la imagen de la base del jugador utilizando la función image()
+
+  Engine.update(engine);
+
+
+  playerBase.display();
+  player.display();
+  playerArcher.display();
+
+  // Título
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("TIRO CON ARCO ÉPICO", width / 2, 100);
+}
